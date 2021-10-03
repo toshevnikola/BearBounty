555c1d8b5cb99b9f8fe9d81377851113c4f3adf5
toshevnikola <toshevnikola@outlook.com>
2021-10-03 12:40:08 +0200

    Added frontend

---
+++ frontend/frontend/src/components/HelloWorld.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <button @click="getRandom()"></button>
+    <p>{{ beautifyRandomList }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  data(): {
+    randomNumberList: Array<number>;
+    testData: string;
+  } {
+    return {
+      testData: "TestData123",
+      randomNumberList: [],
+    };
+  },
+  methods: {
+    getRandom() {
+      let randomNumber: number = Math.random();
+      this.randomNumberList = [...this.randomNumberList, randomNumber];
+    },
+  },
+  computed: {
+    beautifyRandomList(): string {
+      return this.randomNumberList.toString();
+    },
+  },
+  props: {
+    msg: String,
+  },
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+* {
+  color: black;
+}
+</style>
---
+++ frontend/frontend/src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <Button text="Click me" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import HelloWorld from "../components/HelloWorld.vue"; // @ is an alias to /src
+import Button from "../components/Button.vue";
+export default defineComponent({
+  name: "Home",
+  components: {
+    HelloWorld,
+    Button,
+  },
+});
+</script>
