de859216cbf7fb648727b9d8fce4d7aff4aa77f2
toshevnikola <toshevnikola@outlook.com>
2021-10-04 20:32:51 +0200

    Added frontend with class-component views

--- frontend/frontend/src/views/Home.vue
+++ frontend/frontend/src/views/Home.vue
@@ -1,19 +1,18 @@
 <template>
   <div class="home">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-    <Button text="Click me" />
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "../components/HelloWorld.vue"; // @ is an alias to /src
-import Button from "../components/Button.vue";
-export default defineComponent({
-  name: "Home",
+import { Options, Vue } from 'vue-class-component';
+import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+
+@Options({
   components: {
     HelloWorld,
-    Button,
   },
-});
+})
+export default class Home extends Vue {}
 </script>
